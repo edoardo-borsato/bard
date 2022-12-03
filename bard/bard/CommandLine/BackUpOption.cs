3ea36b90605faba892e1ee9093cca4fb4e7fd1a3
edoardo.borsato <edoardo.borsato@imagicle.com>
2022-12-03 13:24:43 +0100

    feat: change aws-profile option short name to 'p'

--- bard/bard/CommandLine/BackUpOption.cs
+++ bard/bard/CommandLine/BackUpOption.cs
@@ -12,7 +12,7 @@ internal record BackUpOption
     [Option('f', "file", Required = true, HelpText = "The backup JSON file full path")]
     public string FilePath { get; init; }
 
-    [Option('a', "aws-profile", Required = true, HelpText = "The AWS profile to use")]
+    [Option('p', "aws-profile", Required = true, HelpText = "The AWS profile to use")]
     public string AwsProfile { get; init; }
 
     [Option('r', "aws-region", Required = false, HelpText = "The AWS region containing the table. If not provided it will be used the AWS profile default region")]
