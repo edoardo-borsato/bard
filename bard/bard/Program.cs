260b18f9cd9b01c6071dca30d840f160dd1ceb9c
edoardo.borsato <edoardo.borsato@imagicle.com>
2024-11-27 18:12:57 +0100

    doc: update doc

--- bard/bard/Program.cs
+++ bard/bard/Program.cs
@@ -2,7 +2,6 @@
 using Amazon.DynamoDBv2;
 using Amazon.DynamoDBv2.Model;
 using Amazon.Runtime.CredentialManagement;
-using Amazon.Runtime.Internal.Transform;
 using bard.CommandLine;
 using bard.Utility;
 using CommandLine;
