759514f3777c0fef3d2b1ce98353c45c6c81e6a2
edoardo.borsato <edoardo.borsato@imagicle.com>
2025-02-25 14:49:02 +0100

    feat(BackUp): make BackUp be able to both query and scan

--- bard/bard/CommandLine/BackUpOption.cs
+++ bard/bard/CommandLine/BackUpOption.cs
@@ -16,4 +16,25 @@ internal record BackUpOption
 
     [Option('r', "aws-region", Required = false, HelpText = "The AWS region containing the table. If not provided it will be used the AWS profile default region")]
     public string? AwsRegion { get; init; }
+
+    [Option('m', "method", Required = true, HelpText = "The AWS Dynamo DB method to use. Possible values are `scan` or `query`. In case of `query` partition key and sort key info are required")]
+    public string? Method { get; init; }
+
+    [Option("partition-key-name", Required = false, HelpText = "The partition key name")]
+    public string? PartitionKeyName { get; init; }
+
+    [Option("partition-key-type", Required = false, HelpText = "The partition key type. Supported types are `B` for binary, `S` for string and `N` for number")]
+    public string? PartitionKeyType { get; init; }
+
+    [Option("partition-key-value", Required = false, HelpText = "The partition key value")]
+    public string? PartitionKeyValue { get; init; }
+
+    [Option("sort-key-name", Required = false, HelpText = "The sort key name")]
+    public string? SortKeyName { get; init; }
+
+    [Option("sort-key-type", Required = false, HelpText = "The sort key type. Supported types are `B` for binary, `S` for string and `N` for number")]
+    public string? SortKeyType { get; init; }
+
+    [Option("sort-key-value", Required = false, HelpText = "The sort key value")]
+    public string? SortKeyValue { get; init; }
 }
